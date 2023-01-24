fac9000b38ba93c9339e624a86c479fdd2175694
Bakai <bakayakylbekov3@gmail.com>
2023-01-24 14:40:32 +0600

    179. useReducer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState, useEffect, useCallback, useMemo} from 'react';
+import {useState, useEffect, useCallback, useMemo, useReducer} from 'react';
 import {NavLink} from 'react-router-dom';
 import {Container} from 'react-bootstrap';
 
@@ -6,80 +6,40 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 import Form from './Form';
 import AppForm from './Forms';
-
-
 import './App.css';
-// class Slider extends Component {
 
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             autoplay: false,
-//             slide: 0
-//         }
-//     }
-
-//     componentDidMount(){
-//         document.title = `Slide: ${this.state.slide}`;
-//     }
-
-//     componentDidUpdate(){
-//         document.title = `Slide: ${this.state.slide}`;
-//     }
-
-//     changeSlide = (i) => {
-//         this.setState(({slide}) => ({
-//             slide: slide + i
-//         }))
-//     }
-
-//     toggleAutoplay = () => {
-//         this.setState(({autoplay}) => ({
-//             autoplay: !autoplay
-//         }))
-//     }
-
-//     render() {
-//         return (
-//             <Container>
-//                 <div className="slider w-50 m-auto">
-//                     <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-//                     <div className="text-center mt-5">Active slide {this.state.slide} <br/> {this.state.autoplay ? 'auto' : null}</div>
-//                     <div className="buttons mt-3">
-//                         <button 
-//                             className="btn btn-primary me-2"
-//                             onClick={() => this.changeSlide(-1)}>-1</button>
-//                         <button 
-//                             className="btn btn-primary me-2"
-//                             onClick={() => this.changeSlide(1)}>+1</button>
-//                         <button 
-//                             className="btn btn-primary me-2"
-//                             onClick={this.toggleAutoplay}>toggle autoplay</button>
-//                     </div>
-//                 </div>
-//             </Container>
-//         )
-//     }
-// }
-
-// const calcValue = () => {
-//     console.log('Random');
-
-//     return Math.random() * (50 - 1) + 1;
-// }
 
 const countTotal = (num) =>{
     console.log('counting...');
     return num + 10;
 }
 
+function reducer (state, action) {
+    switch (action.type){
+        case 'toggle':
+            return {autoplay: !state.autoplay};
+        case 'slow':
+            return {autoplay: 300};
+        case 'fast':
+            return {autoplay: 700};
+        case 'custom':
+            return {autoplay: action.payload};
+        default:
+            throw new Error();
+    }
+}
 
-const Slider = (props) => {
+function init(initial){
+    return {autoplay: initial}
+}
+
+const Slider = ({initial}) => {
 
     const [slide, setSlide]= useState(0);
-    const [autoplay, setAutoplay] = useState(false);
+    // const [autoplay, setAutoplay] = useState(false);
+    const [autoplay, dispatch] = useReducer(reducer, initial, init);
 
-    const getSomeImages = useCallback(() =>{
+    const getSomeImages = useCallback(() =>{ 
         console.log('fetching');
         return[
             "https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg",
@@ -113,16 +73,16 @@ const Slider = (props) => {
         setSlide(slide => slide + i)
     }
 
-    function toggleAutoplay(){
-        setAutoplay(autoplay => !autoplay)
-    }
+    // function toggleAutoplay(){
+    //     setAutoplay(autoplay => !autoplay)
+    // }
  
     return (
         <Container>
             <div className="slider w-50 m-auto">
                 <Slide getSomeImages={getSomeImages}/>
 
-                <div className="text-center mt-5">Active slide {slide} <br/> {autoplay ?  'auto' : null}</div>
+                <div className="text-center mt-5">Active slide {slide} <br/> {autoplay.autoplay ?  'auto' : null}</div>
                 <div style={style} className="text-center mt-5">Total slides: {total}</div>
                 <div className="buttons mt-3">
                     <button 
@@ -133,7 +93,16 @@ const Slider = (props) => {
                         onClick={() => changeSlide(1)}>+1</button>
                     <button 
                         className="btn btn-primary me-2"
-                        onClick={toggleAutoplay}>toggle autoplay</button>
+                        onClick={() => dispatch({type: 'toggle'})}>toggle autoplay</button>
+                    <button  
+                        className="btn btn-primary me-2"
+                        onClick={() => dispatch({type: 'slow'})}>slow autoplay</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => dispatch({type: 'fast'})}>fast autoplay</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={(e) => dispatch({type: 'custom', payload: +e.target.textContent})}>1000</button>
                 </div>
             </div>
         </Container>
@@ -174,11 +143,9 @@ function App() {
 
     return (
         <>
-
-
             <Router> 
             <button onClick={() => setSlide(false)}>Click me</button>
-            {slider ? <Slider/> : null}
+            {slider ? <Slider initial={false} /> : null}
                 <Header/>
                 <main>
                     <Routes>
